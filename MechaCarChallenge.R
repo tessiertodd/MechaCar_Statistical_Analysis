5ae70c662e340da6455248e56c34184e3e5771e9
tessiertodd <tessiert@yahoo.com>
2021-12-04 22:52:59 -0500

    Module 15 - Challenge Assignment - Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,7 @@ library(dplyr)
 MechaCar <- read.csv('MechaCar_mpg.csv',check.names=F,stringsAsFactors=F) #import MechaCar as dataset
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar) # generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)) #summarize multiple linear reg mod
+
+SuspensionCoil <- read.csv('Suspension_Coil.csv',check.names=F,stringsAsFactors=F) #import SuspensionCoil as dataset
+total_summary <- summarize(SuspensionCoil,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create summary table of PSI stats
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #create summary table of PSI stats by lot
