affc6f2fb0b8cdd3de744a0fe0d6661c708953e6
tessiertodd <tessiert@yahoo.com>
2021-12-05 00:47:16 -0500

    Module 15 - Challenge Assignment - Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 SuspensionCoil <- read.csv('Suspension_Coil.csv',check.names=F,stringsAsFactors=F) #import SuspensionCoil as dataset
 total_summary <- summarize(SuspensionCoil,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create summary table of PSI stats
 lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #create summary table of PSI stats by lot
+
+t.test((SuspensionCoil$PSI),mu=mean(1500)) #t-test on the 3 lots versus the population mean of 1500
+
+t.test((subset(SuspensionCoil,Manufacturing_Lot=="Lot1")$PSI),mu=mean(1500)) #t-test on lot 1 versus population mean of 1500
+t.test((subset(SuspensionCoil,Manufacturing_Lot=="Lot2")$PSI),mu=mean(1500)) #t-test on lot 2 versus population mean of 1500
+t.test((subset(SuspensionCoil,Manufacturing_Lot=="Lot3")$PSI),mu=mean(1500)) #t-test on lot 3 versus population mean of 1500
